5fb26975d148f57409c9785a6f76ddfb2546e7bd
Dima Kapinus <viktoriakapinus@MacBook-Pro-Viktoria.local>
2022-11-12 12:21:58 +0300

    first commit

---
+++ it-incubator-todolist-ts-01/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+
+function App() {
+
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
